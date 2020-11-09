c922c28f434a9971d2a98d1b9bfa635fe2fb0783
rubinak786 <70865586+rubinak786@users.noreply.github.com>
2020-11-10 03:57:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(500,800);
+    var canvas = createCanvas(1300,400);
     engine = Engine.create();
     world = engine.world;
 
